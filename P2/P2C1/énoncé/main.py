c81cde52b278aa5922ef6b644af727a13df96aa4
BenjaminPapy <benjpaps@gmail.com>
2025-09-10 16:22:17 +0200

    Exercices Python Conditions/Boucles Partie 2

--- P2/P2C1/énoncé/main.py
+++ P2/P2C1/énoncé/main.py
@@ -1,1 +1,37 @@
-# Ecrivez votre code ici !
+#1
+nombre1 = input("Fournissez un premier nombre: ")
+nombre2 = input("Fournissez un second nombre: ")
+
+#2
+if not nombre1.isnumeric() or not nombre2.isnumeric():
+    print("Erreur les nombres doivent être entiers")
+#3
+    raise SystemExit("Fin du programme")
+
+#4
+nombre1 = int(nombre1)
+nombre2 = int(nombre2)
+
+#5
+operation = input("Entrez l'opérateur souhaitée '+', '-', '*' ou '/' : ")
+
+#6
+if operation not in ["+", "-", "*", "/"]:
+    print("L'operateur choisis n'existe pas")
+    raise SystemExit("Fin du programme")
+
+#7,8,9,10
+if operation == "+":
+    resultat = nombre1 + nombre2
+elif operation == "-":
+    resultat = nombre1 - nombre2
+elif operation == "*":
+    resultat = nombre1 * nombre2
+elif operation == "/":
+    if nombre2 == 0:
+        print("Vous ne pouvez pas diviser un chiffre/nombre par 0")
+        raise SystemExit("Fin du programme")
+    
+    resultat = round(nombre1 / nombre2, 2)
+
+print(f"Le resultat du calcul est : {resultat} :)")
--- P2/P2C2/énoncé/main.py
+++ P2/P2C2/énoncé/main.py
@@ -1,1 +1,29 @@
-# Ecrivez votre code ici !
+#1, 2
+nombres = input("Veuillez saisir une liste de plusieurs nombres entier séparés(ex: 1 2 3 4 ...): ")
+
+#3
+reponse = nombres.split()
+
+#4
+liste_entiers = [int(val) for val in reponse]
+    
+#5
+resultat = 0
+for nbr in liste_entiers:
+    resultat += nbr
+ 
+print(f"La somme de votre liste est : {resultat}")
+
+#6
+moyenne = resultat / len(liste_entiers)
+
+print(f"La moyenne de cette liste est : {moyenne}")
+
+#7
+superieur_moyenne = 0
+
+for nbr in liste_entiers:
+    if nbr > moyenne:
+        superieur_moyenne = superieur_moyenne + 1
+
+print(superieur_moyenne)

1b24008eb3244b615255312514da238d2a50dfaa
BenjaminPapy <benjpaps@gmail.com>
2025-09-21 15:58:22 +0200

    Exercices Python Import Partie 3

--- P3/P3C1/énoncé/main.py
+++ P3/P3C1/énoncé/main.py
@@ -1,1 +1,7 @@
-# Ecrivez votre code ici !
+from calculator.operations import addition, multiplication
+
+if __name__ == '__main__':
+    resultat_addition = addition(3, 5)
+    resultat_multiplication = multiplication(8, 2)
+    print("Resultat de votre addition :", resultat_addition)
+    print("Résultat de votre multiplication :", resultat_multiplication)
